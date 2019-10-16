5fdd6c83b0bc04690b873785330ea4f130b0efc7
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-10-16 15:16:35 +0200

    Add users and currencies migration

--- pyexchange/models.py
+++ pyexchange/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class User(models.Model):
+    login = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    token = models.CharField(max_length=5000)
+    money = models.IntegerField(default=0)
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+    unit = models.IntegerField(default=1)
+    purchase_price = models.FloatField(default=0)
+    sell_price = models.FloatField(default=0)
+
+
+class UserCurrency(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
+
+
+
