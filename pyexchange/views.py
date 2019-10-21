42322d6cf2115f8f1cb30e506c19307402b05e5f
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-10-21 18:30:01 +0200

    Correct JWT authentication

--- pyexchange/views.py
+++ pyexchange/views.py
@@ -5,9 +5,11 @@ from rest_framework.mixins import (
 )
 from pyexchange.serializers import CurrencySerializer
 from pyexchange.models import Currency
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class CurrencyViewSet(GenericViewSet, ListModelMixin):
+    #permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
     queryset = Currency.objects.all().order_by('name')
     serializer_class = CurrencySerializer
--- pyexchange_backend/settings.py
+++ pyexchange_backend/settings.py
@@ -42,11 +42,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    #'DEFAULT_PERMISSION_CLASSES': [
-    #    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    #],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
@@ -105,18 +103,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+
 ]
 
 
