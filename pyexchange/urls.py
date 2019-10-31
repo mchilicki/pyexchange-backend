569879618fb34a5b4441b3abb2fa1c662b25393d
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-10-31 18:50:14 +0100

    Correct profile crerating on user creating

--- pyexchange/urls.py
+++ pyexchange/urls.py
@@ -8,13 +8,11 @@ from rest_framework_simplejwt.views import (
 )
 
 router = routers.DefaultRouter()
-router.register(r'currencies', views.CurrencyViewSet)
+router.register(r'api/currencies', views.CurrencyViewSet)
 
 # Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
