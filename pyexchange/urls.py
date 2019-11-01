7bf998d7dfbb5d1e1abc48e97da7ed90fea052ae
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-11-01 14:31:40 +0100

    Add swagger endpoint documentation

--- pyexchange/urls.py
+++ pyexchange/urls.py
@@ -6,11 +6,28 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = routers.DefaultRouter()
 router.register(r'api/currencies', views.CurrencyViewSet)
 router.register(r'api/user-currencies', views.UserCurrencyViewSet)
 router.register(r'api/users', views.UserViewSet)
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 # Wire up our API using automatic URL routing.
 urlpatterns = [
@@ -18,4 +35,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- pyexchange_backend/settings.py
+++ pyexchange_backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pyexchange.apps.PyexchangeConfig',
     'rest_framework',
+    'drf_yasg',
 ]
 
 REST_FRAMEWORK = {
